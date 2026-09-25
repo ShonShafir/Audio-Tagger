7bbe4977782ad146bd86868c2b663a98dbfff600
ShonShafir <shonshafir@github.com>
2026-09-25 14:46:24 +0300

    chore: remove temporary patch and testing files

--- patch_hide2.py
+++
@@ -1,9 +0,0 @@
-import os
-content = open('core/ui/tagger_tab.py', encoding='utf-8').read()
-
-content = content.replace(
-    'self.pause_btn.setText("\\u23f8 Pause")\n        self.progress.setValue(0)',
-    'self.pause_btn.setText("\\u23f8 Pause")\n        self.progress.show()\n        self.progress.setValue(0)'
-)
-
-open('core/ui/tagger_tab.py', 'w', encoding='utf-8').write(content)
